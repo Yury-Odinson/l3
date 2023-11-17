3842843b13de040d14570cb513988a539952d329
yury-odinson <yury.odinson@gmail.com>
2023-11-17 11:07:24 +0300

    feat: favorites empty page

--- src/modules/favorites/favorites.ts
+++ src/modules/favorites/favorites.ts
@@ -7,8 +7,13 @@ export const favorites: any = JSON.parse(localStorage.getItem("favorites") || "[
 class Favorites extends Component {
     products!: ProductData[];
 
+    render() {
+        if (favorites.length <= 0) {
+            this.view.root.classList.add("is__empty");
+            return;
+        }
 
-
+    }
 }
 
 export const favoritesComp = new Favorites(html)
