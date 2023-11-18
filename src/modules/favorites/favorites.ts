047b88a4af4113ae065a2a115d319dafa5ec8805
Yury <yury.odinson@gmail.com>
2023-11-18 13:07:20 +0300

    feat: correct display favorites page

--- src/modules/favorites/favorites.ts
+++ src/modules/favorites/favorites.ts
@@ -2,6 +2,7 @@ import { ProductData } from "types";
 import { Component } from "../component";
 import html from "./favorites.tpl.html";
 import { cartFavService } from '../../services/cartFav.service';
+import { Product } from "../product/product";
 
 class Favorites extends Component {
     products!: ProductData[];
@@ -16,6 +17,13 @@ class Favorites extends Component {
         } else {
             headerFavLink?.classList.remove("favIsEmpty");
         }
+
+        this.products.forEach((product) => {
+            const productComp = new Product(product);
+            productComp.render();
+            productComp.attach(this.view.cartFav);
+        });
+
 
     }
 }
