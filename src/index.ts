cf38bb864cf025acd73e4044a74bc44a4de4f8c4
Yury <yury.odinson@gmail.com>
2023-11-18 20:44:46 +0300

    feat: event change page

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import "./icons";
 import Router from "./router";
 import { cartService } from "./services/cart.service";
+import { eventService } from "./services/event.service";
 import { userService } from "./services/user.service";
 
 new Router();
@@ -10,3 +11,6 @@ userService.init();
 setTimeout(() => {
   document.body.classList.add("is__ready");
 }, 250);
+
+const pageURL = window.location.href;
+eventService.sendEvent("route", { url: pageURL });
