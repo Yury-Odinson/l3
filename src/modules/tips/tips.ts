cdc0c7cf7cbe359c9f5e5483eb1d006c810b3fe0
Yury <yury.odinson@gmail.com>
2023-11-22 23:48:31 +0300

    template structure

--- src/modules/tips/tips.ts
+++ src/modules/tips/tips.ts
@@ -4,18 +4,33 @@ import { ViewTemplate } from '../../utils/viewTemplate';
 
 export class Tips {
     view: View;
-    tips: any;
+    tips: string[];
 
     constructor() {
         this.view = new ViewTemplate(html).cloneView();
+        this.tips = ["Волшебная палочка", "знания js", "вселенский разум"];
     }
 
     attach($root: HTMLElement) {
         $root.innerHTML = '';
         $root.appendChild(this.view.root);
+        this.render();
     }
 
+    private _createTip(tip: string) {
+        const element = document.createElement("span");
+        element.classList.add("tip__item");
+        element.textContent = tip;
+        return element;
+    }
+
+
     render() {
-        ViewTemplate.applyText(this.view.root)
+        this.view.root.innerHTML = "";
+        const test = this._createTip(this.tips[1])
+        console.log(test)
+
+        const result = `Например, ${this.tips[0]}`;
+        this.view.root.innerHTML = result;
     }
 }
