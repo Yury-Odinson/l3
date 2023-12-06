be547a556649bc28c4246f32deb675d2580b839b
Yury <yury.odinson@gmail.com>
2023-12-06 21:58:37 +0300

    redo: add try-catch

--- src/modules/homepage/homepage.ts
+++ src/modules/homepage/homepage.ts
@@ -1,42 +1,48 @@
-import { addElement } from '../../utils/helpers';
-import { Component } from '../component';
+import {addElement} from '../../utils/helpers';
+import {Component} from '../component';
 import html from './homepage.tpl.html';
 
-import { ProductList } from '../productList/productList';
-import { userService } from '../../services/user.service';
+import {ProductList} from '../productList/productList';
+import {userService} from '../../services/user.service';
 
 class Homepage extends Component {
-  popularProducts: ProductList;
+    popularProducts: ProductList;
 
-  constructor(props: any) {
-    super(props);
+    constructor(props: any) {
+        super(props);
 
-    this.popularProducts = new ProductList();
-    this.popularProducts.attach(this.view.popular);
-  }
+        this.popularProducts = new ProductList();
+        this.popularProducts.attach(this.view.popular);
+    }
 
-  render() {
-    userService.init().then(() => {
-      fetch('/api/getPopularProducts', {
-        headers: {
-          'x-userid': window.userId,
+    async render() {
+
+        await userService.init();
+
+        try {
+            fetch('/api/getPopularProducts', {
+                headers: {
+                    'x-userid': window.userId,
+                }
+            })
+                .then((res) => res.json())
+                .then((products) => {
+                    this.popularProducts.update(products);
+                });
+
+            const isSuccessOrder = new URLSearchParams(window.location.search).get('isSuccessOrder');
+            if (isSuccessOrder != null) {
+                const $notify = addElement(this.view.notifies, 'div', {className: 'notify'});
+                addElement($notify, 'p', {
+                    innerText:
+                        'Заказ оформлен. Деньги спишутся с вашей карты, менеджер может позвонить, чтобы уточнить детали доставки'
+                });
+            }
+        } catch (error) {
+            console.log("fetch Error", error);
         }
-      })
-        .then((res) => res.json())
-        .then((products) => {
-          this.popularProducts.update(products);
-        });
 
-      const isSuccessOrder = new URLSearchParams(window.location.search).get('isSuccessOrder');
-      if (isSuccessOrder != null) {
-        const $notify = addElement(this.view.notifies, 'div', { className: 'notify' });
-        addElement($notify, 'p', {
-          innerText:
-            'Заказ оформлен. Деньги спишутся с вашей карты, менеджер может позвонить, чтобы уточнить детали доставки'
-        });
-      }
-    })
-  }
+    }
 }
 
 export const homepageComp = new Homepage(html);
--- src/modules/productDetail/productDetail.ts
+++ src/modules/productDetail/productDetail.ts
@@ -1,70 +1,81 @@
-import { Component } from '../component';
-import { ProductList } from '../productList/productList';
-import { formatPrice } from '../../utils/helpers';
-import { ProductData } from 'types';
+import {Component} from '../component';
+import {ProductList} from '../productList/productList';
+import {formatPrice} from '../../utils/helpers';
+import {ProductData} from 'types';
 import html from './productDetail.tpl.html';
-import { cartService } from '../../services/cart.service';
+import {cartService} from '../../services/cart.service';
+import {userService} from "../../services/user.service";
 
 class ProductDetail extends Component {
-  more: ProductList;
-  product?: ProductData;
+    more: ProductList;
+    product?: ProductData;
 
-  constructor(props: any) {
-    super(props);
+    constructor(props: any) {
+        super(props);
 
-    this.more = new ProductList();
-    this.more.attach(this.view.more);
-  }
+        this.more = new ProductList();
+        this.more.attach(this.view.more);
+    }
 
-  async render() {
-    const urlParams = new URLSearchParams(window.location.search);
-    const productId = Number(urlParams.get('id'));
+    async render() {
+        const urlParams = new URLSearchParams(window.location.search);
+        const productId = Number(urlParams.get('id'));
 
-    const productResp = await fetch(`/api/getProduct?id=${productId}`);
-    this.product = await productResp.json();
-
-    if (!this.product) return;
-
-    const { id, src, name, description, salePriceU } = this.product;
-
-    this.view.photo.setAttribute('src', src);
-    this.view.title.innerText = name;
-    this.view.description.innerText = description;
-    this.view.price.innerText = formatPrice(salePriceU);
-    this.view.btnBuy.onclick = this._addToCart.bind(this);
-
-    const isInCart = await cartService.isInCart(this.product);
-
-    if (isInCart) this._setInCart();
-
-    fetch(`/api/getProductSecretKey?id=${id}`)
-      .then((res) => res.json())
-      .then((secretKey) => {
-        this.view.secretKey.setAttribute('content', secretKey);
-      });
-
-    fetch('/api/getPopularProducts', {
-        headers: {
-          'x-userid': window.userId
+        await userService.init();
+        try {
+            const productResp = await fetch(`/api/getProduct?id=${productId}`, {
+                headers: {
+                    'x-userid': window.userId
+                }
+            });
+            this.product = await productResp.json();
+        } catch (error) {
+            console.log("fetch Error", error);
         }
-      })
-      .then((res) => res.json())
-      .then((products) => {
-        this.more.update(products);
-      });
-  }
 
-  private _addToCart() {
-    if (!this.product) return;
 
-    cartService.addProduct(this.product);
-    this._setInCart();
-  }
+        if (!this.product) return;
 
-  private _setInCart() {
-    this.view.btnBuy.innerText = '✓ В корзине';
-    this.view.btnBuy.disabled = true;
-  }
+        const {id, src, name, description, salePriceU} = this.product;
+
+        this.view.photo.setAttribute('src', src);
+        this.view.title.innerText = name;
+        this.view.description.innerText = description;
+        this.view.price.innerText = formatPrice(salePriceU);
+        this.view.btnBuy.onclick = this._addToCart.bind(this);
+
+        const isInCart = await cartService.isInCart(this.product);
+
+        if (isInCart) this._setInCart();
+
+        fetch(`/api/getProductSecretKey?id=${id}`)
+            .then((res) => res.json())
+            .then((secretKey) => {
+                this.view.secretKey.setAttribute('content', secretKey);
+            });
+
+        fetch('/api/getPopularProducts', {
+            headers: {
+                'x-userid': window.userId
+            }
+        })
+            .then((res) => res.json())
+            .then((products) => {
+                this.more.update(products);
+            });
+    }
+
+    private _addToCart() {
+        if (!this.product) return;
+
+        cartService.addProduct(this.product);
+        this._setInCart();
+    }
+
+    private _setInCart() {
+        this.view.btnBuy.innerText = '✓ В корзине';
+        this.view.btnBuy.disabled = true;
+    }
 }
 
 export const productDetailComp = new ProductDetail(html);
